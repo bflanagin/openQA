bda77781f434e8d1ca261a86a55b17329f59b43e
bflanagin <bflanagin@pop-os.localdomain>
2021-08-23 16:37:26 -0600

    changed relative paths to absolute

--- script/pop-install.sh
+++ script/pop-install.sh
@@ -371,12 +371,12 @@ systemctl restart apache2
 
 echo "Configuring Database"
 
-sudo -u postgres script/setup-db
-sudo -u postgres script/initdb
+sudo -u postgres /usr/share/openqa/script/setup-db
+sudo -u postgres /usr/share/openqa/script/initdb
 
 echo "Cloning tests"
 chown -R system76:www-data /var/lib/openqa/
-sudo -u system76 git clone git@github.com:pop-os/os-autoinst-distri-pop.git /var/lib/openqa/tests/pop
+sudo -u system76 git clone https://github.com/pop-os/os-autoinst-distri-pop.git /var/lib/openqa/tests/pop
 cd /var/lib/openqa/tests/pop
 sudo -u system76 ../script/load_templates ./products/templates
 
